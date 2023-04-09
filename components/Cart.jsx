3202dde1929e87ab91721dd432a8a327c3db4509
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2023-04-09 22:14:36 +0630

    Adding Cart 1.3 (it's getting real)

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -47,6 +47,18 @@ const Cart = () => {
             </Link>
           </div>
         )}
+
+        <div className="product-container">
+          {cartItems.length >= 1 &&
+            cartItems.map((item) => (
+              <div className="product" key={item._id}>
+                <img
+                  src={urlFor(item?.image[0])}
+                  className="cart-product-image"
+                />
+              </div>
+            ))}
+        </div>
       </div>
     </div>
   );
