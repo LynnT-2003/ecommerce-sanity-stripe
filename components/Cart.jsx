9eb6b228d1c9b3943cb772d18a7316bb179adaec
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2023-04-09 22:05:30 +0630

    Adding Cart 1.1

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useRef } from "react";
+import Link from "next/link";
+import {
+  AiOutlineMinus,
+  AiOutlinePlus,
+  AiOutlineLeft,
+  AiOutlineRight,
+} from "react-icons/ai";
+
+import { TiDeleteOutline } from "react-icons/ti";
+import { toast } from "react-hot-toast";
+
+import { useStateContext } from "../context/StateContext";
+import { urlFor } from "../lib/client";
 
 const Cart = () => {
-  return <div>Cart</div>;
+  const cartRef = useRef();
+  const { totalPrice, totalQuantities, cartItems, setShowCart } =
+    useStateContext();
+
+  return (
+    <div className="cart-wrapper" ref={cartRef}>
+      <div className="cart-container">
+        <button
+          tpe="button"
+          className="cart-heading"
+          onClick={() => setShowCart(false)}
+        >
+          <AiOutlineLeft />
+          <span className="heading">Your Cart</span>
+          <span className="cart-num-items">({totalQuantities} items)</span>
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
