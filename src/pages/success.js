da09ae7a23e5e8eab44d93b6403ebbc8e50a6f67
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2023-04-10 16:48:19 +0630

    Fixed naming predicaments

--- src/pages/success.js
+++ src/pages/success.js
@@ -6,7 +6,7 @@ import { useStateContext } from "../../context/StateContext";
 
 import { runFireworks } from "../../lib/utils";
 
-const success = () => {
+const Success = () => {
   const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext();
 
   useEffect(() => {
@@ -41,4 +41,4 @@ const success = () => {
   );
 };
 
-export default success;
+export default Success;
